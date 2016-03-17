a9d8e50b09218752d3ae274e4dbe71f93c267065
twobacs <twobacs@gmail.com>
2016-03-17 11:28:35 +0100

    Create connect.php

---
+++ js/php/connect.php
@@ -0,0 +1,51 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=','','');
+include('../../../class/fournisseurs.class.php');
+
+function htmltosql($text)
+	{
+	$rep=htmlentities($text,ENT_QUOTES, "UTF-8");
+	return $rep;
+	}
+	
+function dateHrfr($date,$dateonly=0) 
+	{
+    $split = explode(" ",$date);
+    $jour = $split[0];
+	$heure = $split[1];
+	
+	$split2 = explode("-",$jour);	
+	$annee = $split2[0];
+    $mois = $split2[1];
+    $jour = $split2[2];
+	if ($dateonly==0)
+		{
+		return $jour."-".$mois."-".$annee." &agrave; ".$heure;
+		}
+	if ($dateonly==1)
+		{
+		return $jour."-".$mois."-".$annee;
+		}
+	}
+	
+function stripAccents($str) {
+    return strtr(utf8_decode($str), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+}
+
+function splitDateTime($date)
+	{
+	$split=explode("T",$date);
+	$jour=$split[0];
+	$heure=$split[1];
+	return $jour.' '.$heure;
+	}
+	
+function getDateFromDateTime($date)
+	{
+	$split=explode(" ",$date);
+	$jour=$split[0];
+	return $jour;
+	}
+
+?>
