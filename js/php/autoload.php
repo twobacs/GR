42907e2561666984e93896e89d31a286b67bd414
twobacs <twobacs@gmail.com>
2016-03-22 15:18:30 +0100

    Update and rename connect.php to autoload.php

--- js/php/autoload.php
+++ js/php/autoload.php
@@ -1,7 +1,18 @@
 <?php
 
 $pdo = new PDO('mysql:host=localhost;dbname=','','');
-include('../../../class/fournisseurs.class.php');
+-include('../../../class/armes.class.php');
+-include('../../../class/articles.class.php');
+-include('../../../class/articles.class.php');
+-include('../../../class/batons.class.php');
+-include('../../../class/brassards.class.php');
+-include('../../../class/ett.class.php');
+-include('../../../class/fournisseurs.class.php');
+-include('../../../class/grades.class.php');
+-include('../../../class/nivacces.class.php');
+-include('../../../class/pers_contact.class.php');
+-include('../../../class/services.class.php');
+-include('../../../class/users.class.php');
 
 function htmltosql($text)
 	{
